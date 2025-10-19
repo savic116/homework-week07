3c1a77a8daf2bf57872d608dcc67425fd3ee73d1
savic116 <savic00085@gmail.com>
2025-10-19 14:48:25 +0900

    feat: Initialize Drift database with Schedules table

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -1,3 +1,14 @@
 import 'package:myapp/week07/calendar_scheduler/model/schedule.dart';
 import 'package:drift/drift.dart';
 
+
+part 'drift_database.g.dart'; // part 파일 지정
+
+@DriftDatabase(
+  table: [
+    Schedules,
+  ],
+)
+
+class LocalDatabase extends _$LocalDatabase{}
+// code generation으로 생성할 클래스 상속
