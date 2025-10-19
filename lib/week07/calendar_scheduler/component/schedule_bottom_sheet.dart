273125bdf78289a184a68c121de2672a00365397
savic116 <savic00085@gmail.com>
2025-10-19 15:45:26 +0900

    feat: Implement schedule creation UI in bottom sheet

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -22,50 +22,51 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet>{
     return Form(
       key: formKey,
       child: SafeArea(
-      child: Container(
-        height: MediaQuery.of(context).size.height / 2 + bottomInset,
-        color: Colors.white,
-        child: Padding(
-          padding:  EdgeInsets.only(left: 8, right: 8, top: 8, bottom: bottomInset),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: CustomTextField(
-                      label: '시작 시간',
-                      isTime: true,
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          color: Colors.white,
+          child: Padding(
+            padding:  EdgeInsets.only(left: 8, right: 8, top: 8, bottom: bottomInset),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        label: '시작 시간',
+                        isTime: true,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
-                    child: CustomTextField(
-                      label: '종료 시간',
-                      isTime: true,
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: CustomTextField(
+                        label: '종료 시간',
+                        isTime: true,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8.0),
-              Expanded(
-                child: CustomTextField(
-                  label: '내용',
-                  isTime: false,
+                  ],
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  // 저장 버튼
-                  onPressed: onSavePressed,
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: PRIMARY_COLOR,
+                SizedBox(height: 8.0),
+                Expanded(
+                  child: CustomTextField(
+                    label: '내용',
+                    isTime: false,
                   ),
-                  child: Text('저장'),
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    // 저장 버튼
+                    onPressed: onSavePressed,
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: PRIMARY_COLOR,
+                    ),
+                    child: Text('저장'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
