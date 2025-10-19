2824232bd4d0a735c2af742849a2b64b4a6dea66
savic116 <savic00085@gmail.com>
2025-10-19 15:43:26 +0900

    Fix: Initialize formKey correctly

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -9,7 +9,7 @@ class ScheduleBottomSheet extends StatefulWidget{
 }
 
 class _ScheduleBottomSheetState extends State<ScheduleBottomSheet>{
-  final GlobalKey<FormState> formKey = G
+  final GlobalKey<FormState> formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     // 키보드 높이 가져오기
