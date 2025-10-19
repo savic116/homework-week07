62909919b18663a820f159d2d3e3fea55cde7d8a
savic116 <savic00085@gmail.com>
2025-10-19 14:54:25 +0900

    feat: Implement schedule creation and deletion

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -12,8 +12,14 @@ part 'drift_database.g.dart'; // part 파일 지정
 
 class LocalDatabase extends _$LocalDatabase{
   Stream<List<Schedule>> watchSchedules(DateTime date) =>
-    (select(Schedules)..where((tbl) => tbl.date.equals(date))).watch();
+    (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
   
   Future<int> creatSchedule(SchedulesCompanion data) =>
-    into(schedules)
+    into(schedules).insert(data);
+  
+  Future<int> removeSchedule(int id) =>
+   (delete(Schedules)..where((tbl) => tbl.id.equals(id))).go();
+
+  @override
+  int get schemaVersion => 1;
 }
