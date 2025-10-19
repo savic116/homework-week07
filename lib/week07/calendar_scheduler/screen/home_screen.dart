e7b88298fc633c46a9115a929afe3e3f9a5e38c6
savic116 <savic00085@gmail.com>
2025-10-19 16:57:59 +0900

    feat: Import GetIt package for dependency injection

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -7,6 +7,8 @@ import 'package:myapp/week07/calendar_scheduler/component/today_banner.dart';
 import 'package:myapp/week07/calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
 import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
+
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({Key? key}) : super (key: key);
