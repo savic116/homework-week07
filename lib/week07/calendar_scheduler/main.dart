e97e22cab67e304dcb2db78f578850fe1c6be19b
savic116 <savic00085@gmail.com>
2025-10-19 14:59:57 +0900

    feat: Initialize Flutter and database setup in main.dart

---
+++ lib/week07/calendar_scheduler/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await init
+
+}
